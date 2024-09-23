5708579eb9e1fa6a87702d59e4eac4944a20dc39
pgonzalesmv <pgonzalesmv@gmail.com>
2024-09-23 11:16:59 -0500

    Creando el componente Gallery

---
+++ src/components/Gallery.js
@@ -0,0 +1,40 @@
+import { useState, useEffect } from "react";
+
+const API_URL = "https://api.unsplash.com/search/photos";
+const ACCESS_KEY = "g2y6RdkJ2pOk3V0Zb-FIXtMq_-TdtPmzTnLpPf5vsco";
+
+function Gallery(){
+    const[images, setImages] = useState([]);
+    const [query, setQuery] = useState("nature");
+    useEffect(() =>{
+        fetchImages();
+    },[]);
+
+    const fetchImages = async () => {
+        const response = await fetchImages(`${API_URL}?query=${query}&client_id=${ACCESS_KEY}`);
+        const data = await response.json();
+        setImages(data.results);
+    }
+    return(
+        <div>
+            <h1>Galería de Imágenes</h1>
+            <input 
+              type="text" 
+              value={query} 
+              onChange={(e) => setQuery(e.target.value)} 
+              placeholder="Busca imágenes..."
+            />
+            <button onClick={fetchImages}>Buscar</button>
+        
+            <div className="gallery">
+              {images.map((image) => (
+                <div key={image.id} className="gallery-item">
+                  <img src={image.urls.small} alt={image.alt_description} />
+                </div>
+              ))}
+            </div>
+          </div>
+        );        
+}
+
+export default Gallery;
