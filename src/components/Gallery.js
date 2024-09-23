66c0da83be71ac6b6752ca7d4662ae5e8ef38f09
pgonzalesmv <pgonzalesmv@gmail.com>
2024-09-23 13:04:36 -0500

    api key

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 
 const API_URL = "https://api.unsplash.com/search/photos";
-const ACCESS_KEY = "g2y6RdkJ2pOk3V0Zb-FIXtMq_-TdtPmzTnLpPf5vsco";
+const ACCESS_KEY = "aqui debes poner tu API KEY de Unsplash";
 
 function Gallery(){
     const [images, setImages] = useState([]);
